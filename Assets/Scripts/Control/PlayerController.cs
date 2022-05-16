eda00105865b83a54c6b8d45500b849b0899bc46
Alexinio97 <alex.andricsak@gmail.com>
2022-05-17 00:16:41 +0300

    Implemented cursors.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,14 +2,35 @@ using UnityEngine;
 using RPG.Movement;
 using RPG.Combat;
 using RPG.Attributes;
+using System;
+using UnityEngine.EventSystems;
 
 namespace RPG.Control
 {
+    // TODO: move cursor mapping login in a separate class
     public class PlayerController : MonoBehaviour
     {
         private Mover _moverScript;
         private Fighter _figther;
         private Health _health;
+
+
+        enum CursorType
+        {
+            None,
+            Movement,
+            Combat,
+            UI
+        }
+
+        [Serializable]
+        struct CursorMapping
+        {
+            public CursorType type;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+        [SerializeField] CursorMapping[] cursorMappings = null;
 
         private void Awake()
         {
@@ -20,9 +41,28 @@ namespace RPG.Control
 
         void Update()
         {
-            if (_health.IsDead) return;
+            if (InteractWithUI()) return;
+            if (_health.IsDead)
+            {
+                SetCursor(CursorType.None);
+                return;
+            }
+
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
+
+            SetCursor(CursorType.None);
+        }
+
+        private bool InteractWithUI()
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                SetCursor(CursorType.UI);
+                return true;
+            }
+
+            return false;
         }
 
         private bool InteractWithCombat()
@@ -41,9 +81,28 @@ namespace RPG.Control
                 {
                     _figther.Attack(combatTarget.gameObject);
                 }
+                SetCursor(CursorType.Combat);
                 return true;
             }
             return false;
+        }
+
+        private void SetCursor(CursorType type)
+        {
+            var mapping = GetCursorMapping(type);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType type)
+        {
+            foreach (var mapping in cursorMappings)
+            {
+                if (mapping.type == type)
+                {
+                    return mapping;
+                }
+            }
+            return new CursorMapping();
         }
 
         private bool InteractWithMovement()
@@ -55,6 +114,7 @@ namespace RPG.Control
                 {
                     _moverScript.StartMoveAction(raycastHit.point, 1f);
                 }
+                SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
